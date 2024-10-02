ae7b7a612119b817872ed5e329e6bd9ce9ceb5f9
rizwan <rizwanfahri123@gmail.com>
2024-10-02 18:43:00 +0700

    rumus trapesium

---
+++ trapesium.py
@@ -0,0 +1,8 @@
+print('********************')
+print('menghitung trapesium')
+print('********************')
+a=int(input('masukan sisi sejajar:\t'))
+b=int(input('masukan sisi sejajar:\t'))
+t=int(input('masukan nilai tinggi:\t'))
+hasil=1/2 * a + b * t
+print("hasil penjumlahannya adalah",hasil)
