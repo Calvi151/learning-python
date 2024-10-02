64958da0232b81c507d85bee5d46154a2226cdc7
rizwan <rizwanfahri123@gmail.com>
2024-10-02 18:52:10 +0700

    rumus luas persegi

---
+++ luas persegi.py
@@ -0,0 +1,7 @@
+print('----------------------------')
+print('---menghitung luas persegi--')
+print('----------------------------')
+
+sisi=int(input('masukan nilai sisi\t'))
+total=sisi * sisi
+print('----hasil penjumlahannya--\t',total)
