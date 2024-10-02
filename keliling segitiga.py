4e9fd53bb8e75a32411d282fa073e345fdb8b0ee
rizwan <rizwanfahri123@gmail.com>
2024-10-02 19:17:46 +0700

    rumus keliling segitiga

---
+++ keliling segitiga.py
@@ -0,0 +1,10 @@
+print('****************************')
+print('menghitung keliling segitiga')
+print('****************************')
+
+a=int(input('masukan nilai panjang sisi 1:\t'))
+b=int(input('msaukan nilai panjang sisi 2:\t'))
+c=int(input('masukan nilai panjang sisi 3:\t'))
+
+hasil= a + b + c
+print('hasil penjumlahannya adalah:\t',hasil)
