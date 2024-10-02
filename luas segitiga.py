b734346e41842bebb9efe25ecb60b505ef7171f6
rizwan <rizwanfahri123@gmail.com>
2024-10-02 19:28:57 +0700

    rumus luas segitiga

---
+++ luas segitiga.py
@@ -0,0 +1,10 @@
+print('------------------------')
+print('menghitung luas segitiga')
+print('------------------------')
+
+a= 1/2
+o=int(input('masukan nilai alas:\t'))
+r=int(input('masukna nilaii tinggi:\t'))
+
+hasil= a * o + r
+print('hasil penjumlahannya adalah:\t',hasil)
