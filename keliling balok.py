092984d085f4e85f8f9b55d0458e687bba0bc737
rizwan <rizwanfahri123@gmail.com>
2024-10-02 19:10:52 +0700

    rumus keliling balok

---
+++ keliling balok.py
@@ -0,0 +1,10 @@
+print('=========================')
+print('==menghitung luas balok==')
+print('=========================')
+
+panjang=int(input('masukan nilai panjang:\t'))
+lebar=int(input('masukan nilai sisi:\t'))
+tinggi=int(input('masukan nilai tinggi:\t'))
+
+hasil= panjang * lebar * tinggi
+print('hasil penjumlahan:\t',hasil)
