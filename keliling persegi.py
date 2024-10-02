2fb0947bada4e03effb539806137066d908f5a65
rizwan <rizwanfahri123@gmail.com>
2024-10-02 19:03:33 +0700

    rumus keliling persegi

---
+++ keliling persegi.py
@@ -0,0 +1,7 @@
+print('===========================')
+print('==menghitung luas persegi==')
+print('===========================')
+
+sisi=int(input('masukan nilai sisi:\t'))
+hasil=4 * sisi
+print('hasil penjumlahan adalah:\t',hasil)
